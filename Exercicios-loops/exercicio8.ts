14ea3b5d5b6afdd333ffa1c3ef412b8841d76f4f
emilycavalcanti <emilycalvacanti.s@gmail.com>
2024-04-16 13:10:24 -0300

    tentativa resolucao de exercicio 8

--- Exercicios-loops/exercicio8.ts
+++ Exercicios-loops/exercicio8.ts
@@ -1,43 +1,38 @@
 //8-Faça um programa (o usuário deverá informar a quantidade de números que será informada)e ao final diga qual o valor do maior e do menor número digitado.
 
-import * as readline from 'readline';
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 
-function usuarioInforma(){
-  const usuario = new Array();
-  let contador = 0;
-  let entrada = rl.question('Digite a quantidade de números a serem testados:', (resposta) => {
-    const quantidade = Number(entrada);
-    if (isNaN(quantidade)){
-      console.log('Isso não é um número válido');
+/*function receberMaiorMenor(m:number,n:){
+    
+    let contador = 0;
+    let lista : number[] = [n];
+
+    for (let i = 0; i < m; i++ ){
+      let numero: number = 
+        
+        if(contador %2 ==0){
+
+             console.log(contador);     
+             quantidadeNumeros++
+    }    
+
+        contador++
     }
-    else {
-      console.log(`Você poderá digitar ${quantidade} números`)
-            while(contador<=quantidade){
-                let numero = rl.question('Diga um número: ',(resposta) => {
-                    usuario.push(numero);
-                })
-                contador ++
-            }
-            Array.isArray(usuario)
-            console.log(usuario);
-        }
-      })
-      rl.close();
-      
+//    console.log(`A quantidade de números pares impressos foi ${quantidadeNumeros}`);
+}
+receberMaiorMenor(1,11);
+
+//Valor retornado: Ana Roberta
+/*nomes = ["Alberto", "Alex", "Sandro", "Ana Roberta"];
+tamanhoNome = 0;
+nome = "";
+
+function maiorNome(item, indice) {
+    if (nomes[indice].length > tamanhoNome) {
+        nome = nomes[indice];
+        tamanhoNome = nomes[indice].length;
+    }
 }
 
-/*rl.question('Digite um número: ', (resposta) => {
-  const numero = Number(resposta);
-  if (isNaN(numero)) {
-    console.log('Isso não é um número válido.');
-  } else {
-    console.log(`Você digitou o número: ${numero}`);
-  }
-  
-});*/
+nomes.forEach(maiorNome);
+console.log(nome);*/
---
+++ Exercicios-loops/exercicio9.ts
@@ -0,0 +1,39 @@
+//Faça um programa que leia N numeros e ao final mostre os 3 maiores numeros digitados
+
+
+/*let maioresNumeros: number = 0;
+
+function lerMaioresNumeros(numerosDigitados){
+    
+    let contador = Number(m+1);
+    
+    while (contador < numeroDois){
+        
+        if(contador %2 ==0){
+            
+             console.log(contador);     
+             quantidadeNumeros++
+    }    
+
+    contador++
+}
+console.log(`A quantidade de números pares impressos foi ${quantidadeNumeros}`);
+}
+let numerosDigitados: number[] = [14,2,6];
+
+receberNumeroPar(1,11);
+
+// Editando pouca coisa no código da pergunta
+function contem(){
+    for(var i = 0; i < array.length; i++){
+      if (array[i] == y){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  var array = [1, 2, 3, 4, 5]
+  console.log(contem(array, 2))
+  console.log(contem(array, 4))
+  console.log(contem(array, 6))
